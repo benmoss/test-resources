8ebb96dda2b38f06ce713aa107fbb8e7a0ec9d1c
Ben Moss <benm@vmware.com>
2022-05-13 10:19:23 -0400

    make tgzs too

--- create-release.sh
+++ create-release.sh
@@ -5,7 +5,9 @@ tag="$1"
 
 for os in linux darwin; do
     for arch in amd64 arm64; do
-        echo "test-${tag}-${os}-${arch}" > "test-${os}-${arch}"
+        name="test-${tag}-${os}-${arch}"
+        echo "${name}" > "${name}"
+        tar -czvf "${name}.tgz" "${name}"
     done
 done
 
